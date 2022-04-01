c122fbe7899db02a00ea71bc137926d46548108f
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-01 10:51:18 -0500

    Added Thought.js, still need to bring in imports for thought and user models and create index.js, that is next

--- models/User.js
+++ models/User.js
@@ -3,35 +3,39 @@ const UserSchema = new Schema(
         username: {
             type: String,
             unique: true,
-            required: true,
+            required: 'Username is required',
             trim: true
         },
         email: {
             type: String,
-            required: true,
+            required: 'Provide a valid email address',
             unique: true,
             validation: [validateEmail, 'Email address is required'],
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
         },
-        thoughts: {
-            _id: [ThoughtSchema]
-        },
-        friends: {
-            _id: [UserSchema]
-        }
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+      toJSON: {
+        virtuals: true
+      },
+      id: false
     }
-    // ,
-    // {
-    //   toJSON: {
-    //     virtuals: true,
-    //     getters: true
-    //   },
-    //   id: false
-    // }
 )
 
 UserSchema.virtual('friendCount').get(function() {
     return this.friends.length;
 })
 
-module.exports = UserSchema;
+module.exports = User;
