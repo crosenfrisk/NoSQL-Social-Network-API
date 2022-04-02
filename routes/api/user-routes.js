eaebe33864a591791de708e66907f2bc2908ee4c
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-02 12:26:54 -0500

    added assets/mockup gifs for reference on API routes. Writing user-routes.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,20 +1,27 @@
-// routes for users:  /api/users
+const router = require('express').Router();
+const {
+    getAllUsers, 
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
 
-// GET all users
+// routes for users:  /api/users
+// Get all users, create a new user
+router.route('/').get(getAllUsers).post(createUser);
 
 // GET a single user by its _id and populated thought and friend data
-
-// POST a new user
-
-// PUT to update a user by its _id
-
-// DELETE to remove user by its _id
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
 
 // BONUS: Remove a user's associated thoughts when deleted.
 
 
 // routes for user's friend list:  /api/users/:userId/friends/:friendId
-
 // POST to add a new friend to a user's friend list
-
 // DELETE to remove a friend from a user's friend list
+router.route('./userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
